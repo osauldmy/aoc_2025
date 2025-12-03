fe513ad46010eb11ff2bc93c7c2f43002c5ca6b7
Dmytro Osaulenko <osauldmy@gmail.com>
2025-12-03 22:04:39 +0100

    Add day 3 2nd part solution

--- aoc/day03.clj
+++ aoc/day03.clj
@@ -7,7 +7,7 @@
 (defn combinations-fix [value n]
   (map #(str/join (map second %1)) (combinatorics/combinations (map-indexed vector (str/split value #"")) n)))
 
-(defn find-largest-joltage [line]
+(defn find-largest-joltage-part-1 [line]
   (apply max
          (map #(Integer/parseInt (str/join %1))
               (combinations-fix line 2))))
@@ -16,10 +16,35 @@
   (->>
    puzzle-input
    str/split-lines
-   (map find-largest-joltage)
+   (map find-largest-joltage-part-1)
+   (reduce +)))
+
+(defn remove-single-digit-keep-max [value]
+  (let [as-list (str/split value #"")]
+    (str (apply max
+                (map #(BigInteger. (str/join %1))
+                     (for [i (range 0 (count as-list))] (assoc as-list i "")))))))
+
+(defn- keep-largest-12-digit-joltage [n value]
+  (if (zero? n)
+    value
+    (keep-largest-12-digit-joltage
+      (dec n)
+      (remove-single-digit-keep-max value))))
+
+(defn find-largest-joltage-part-2 [value]
+  (keep-largest-12-digit-joltage (- (count value) 12) value))
+
+(defn part-2 [puzzle-input]
+  (->>
+   puzzle-input
+   str/split-lines
+   (map find-largest-joltage-part-2)
+   (map Long/parseLong)
    (reduce +)))
 
 #_(do
     (def puzzle-sample-input (slurp "inputs/03_sample.txt"))
     (def puzzle-input (slurp "inputs/03.txt"))
-    (println "Part 1:" (time (part-1 puzzle-input))))
+    (println "Part 1:" (time (part-1 puzzle-input)))
+    (println "Part 2:" (time (part-2 puzzle-input))))
