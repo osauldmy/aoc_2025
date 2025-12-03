1f377595adc38539956259583619f3c527bafd3d
Dmytro Osaulenko <osauldmy@gmail.com>
2025-12-03 22:24:01 +0100

    refactor: day 3 to use same function for part1 and part2

--- aoc/day03.clj
+++ aoc/day03.clj
@@ -1,23 +1,5 @@
 (ns day03
-  (:require [clojure.string :as str]
-            [clojure.math.combinatorics :as combinatorics]))
-
-;; NOTE: clojure.math.combinatorics/combinations actually skips duplicates in a way I wouldn't expect
-;; itertools.combinations gives correct solution without "fixing it for me"
-(defn combinations-fix [value n]
-  (map #(str/join (map second %1)) (combinatorics/combinations (map-indexed vector (str/split value #"")) n)))
-
-(defn find-largest-joltage-part-1 [line]
-  (apply max
-         (map #(Integer/parseInt (str/join %1))
-              (combinations-fix line 2))))
-
-(defn part-1 [puzzle-input]
-  (->>
-   puzzle-input
-   str/split-lines
-   (map find-largest-joltage-part-1)
-   (reduce +)))
+  (:require [clojure.string :as str]))
 
 (defn remove-single-digit-keep-max [value]
   (let [as-list (str/split value #"")]
@@ -25,21 +7,29 @@
                 (map #(BigInteger. (str/join %1))
                      (for [i (range 0 (count as-list))] (assoc as-list i "")))))))
 
-(defn- keep-largest-12-digit-joltage [n value]
+(defn- keep-largest-n-digit-joltage [n value]
   (if (zero? n)
     value
-    (keep-largest-12-digit-joltage
+    (keep-largest-n-digit-joltage
       (dec n)
       (remove-single-digit-keep-max value))))
 
-(defn find-largest-joltage-part-2 [value]
-  (keep-largest-12-digit-joltage (- (count value) 12) value))
+(defn find-largest-joltage [n value]
+  (keep-largest-n-digit-joltage (- (count value) n) value))
+
+(defn part-1 [puzzle-input]
+  (->>
+   puzzle-input
+   str/split-lines
+   (map (partial find-largest-joltage 2))
+   (map Integer/parseInt)
+   (reduce +)))
 
 (defn part-2 [puzzle-input]
   (->>
    puzzle-input
    str/split-lines
-   (map find-largest-joltage-part-2)
+   (map (partial find-largest-joltage 12))
    (map Long/parseLong)
    (reduce +)))
 
