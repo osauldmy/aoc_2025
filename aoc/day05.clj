4f8ffd0d523395be92f00179f69a1fcf2f75d306
Dmytro Osaulenko <osauldmy@gmail.com>
2025-12-05 14:26:06 +0100

    Add 2nd part solution for day 5

--- aoc/day05.clj
+++ aoc/day05.clj
@@ -15,7 +15,28 @@
    (map str/split-lines)
    solve-part-1))
 
+(defn merge-intervals [intervals]
+  (reduce
+   (fn [acc [x y]]
+     (let [[a b] (last acc)]
+       (cond
+         (empty? acc) [[x y]]
+         (>= b x) (conj (into [] (butlast acc)) [a (max b y)])
+         :else (conj acc [x y]))))
+   [] (sort-by first intervals)))
+
+(defn part-2 [puzzle-input]
+  (->>
+   (str/split puzzle-input #"\n\n")
+   (map str/split-lines)
+   first
+   (map #(map Long/parseLong (str/split % #"-")))
+   merge-intervals
+   (map (fn [[a b]] (- (inc b) a)))
+   (reduce +)))
+
 #_(do
     (def puzzle-sample-input (slurp "inputs/05_sample.txt"))
     (def puzzle-input (slurp "inputs/05.txt"))
-    (println "Part 1:" (time (part-1 puzzle-input))))
+    (println "Part 1:" (time (part-1 puzzle-input)))
+    (println "Part 2:" (time (part-2 puzzle-input))))
