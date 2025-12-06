b5eb3def3e5e9e6ac9f034698c6f30d200ab64da
Dmytro Osaulenko <osauldmy@gmail.com>
2025-12-06 14:48:17 +0100

    Add 2nd part of day 6

--- aoc/day06.clj
+++ aoc/day06.clj
@@ -11,7 +11,25 @@
    (map #(apply (if (= (last %) "*") * +) (map Integer/parseInt (butlast %))))
    (reduce +)))
 
+(defn part-2 [puzzle-input]
+  (let [lines (str/split-lines puzzle-input)
+        operations (map #(if (= "*" %) * +) (str/split (last lines) #"\s+"))
+        columns (->> (map #(str/split % #"") (butlast lines))
+                     (apply map vector)
+                     (map #(str/join "" %)))
+        partitioned (filter #(not (str/blank? (first %)))
+                            (partition-by str/blank? columns))
+        cleaned (map
+                  (fn [lst]
+                    (map #(Integer/parseInt (str/trim %)) lst)) partitioned)]
+      (->> cleaned
+           (map-indexed
+             (fn [index items]
+               (apply (nth operations index) items)))
+           (reduce +))))
+
 #_(do
     (def puzzle-sample-input (slurp "inputs/06_sample.txt"))
     (def puzzle-input (slurp "inputs/06.txt"))
-    (println "Part 1:" (time (part-1 puzzle-input))))
+    (println "Part 1:" (time (part-1 puzzle-input)))
+    (println "Part 2:" (time (part-2 puzzle-input))))
