6fa5cedc4f448baab7cbea87043d42bd43efc12c
Dmytro Osaulenko <osauldmy@gmail.com>
2025-12-11 13:51:10 +0100

    Add 2nd part of day 11

--- day11.clj
+++ day11.clj
@@ -19,7 +19,27 @@
        flatten
        count))
 
+(def solve-2
+  (memoize
+   (fn [has-fft has-dac k m]
+     (cond
+       (and (= k "out") has-fft has-dac) 1
+       (not= (contains? m k)) (throw (AssertionError. "Unreachable branch"))
+       :else (reduce +
+                     (map #(solve-2 (or has-fft (= % "fft")) (or has-dac (= % "dac")) % m) (get m k)))))))
+
+(defn part-2 [puzzle-input]
+  (->> puzzle-input
+       str/split-lines
+       (map #(str/split % #": "))
+       (reduce
+        (fn [m [k v]]
+          (assoc m k (str/split v #" "))) {})
+       (solve-2 false false "svr")))
+
 #_(do
     (def puzzle-sample-input (slurp "inputs/11_sample.txt"))
+    (def puzzle-sample-input2 (slurp "inputs/11_sample2.txt"))
     (def puzzle-input (slurp "inputs/11.txt"))
-    (println "Part 1:" (time (part-1 puzzle-input))))
+    (println "Part 1:" (time (part-1 puzzle-input)))
+    (println "Part 2:" (time (part-2 puzzle-input))))
